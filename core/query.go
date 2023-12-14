9cca97f45d2b53211db8efed322413643562428d
NinjaPhillipe <23127972+NinjaPhillipe@users.noreply.github.com>
2023-12-14 06:13:19 +0100

    query

--- core/query.go
+++ core/query.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 )
 
+// tags
+
 func (sqliteDb *SqliteDb) SelectAllTags() []Tag {
-	rows, err := sqliteDb.db.Query("SELECT * FROM tags")
+	rows, err := sqliteDb.db.Query("SELECT * FROM tag")
 	if err != nil {
 		panic(err)
 	}
@@ -16,7 +18,7 @@ func (sqliteDb *SqliteDb) SelectAllTags() []Tag {
 }
 
 func (sqliteDb *SqliteDb) SelectTag(tagId int) (*Tag, error) {
-	row, err := sqliteDb.db.Query("SELECT * FROM tags WHERE tag_id = ?", tagId)
+	row, err := sqliteDb.db.Query("SELECT * FROM tag WHERE tag_id = ?", tagId)
 	if err != nil {
 		// panic(err)
 		return nil, err
@@ -28,17 +30,56 @@ func (sqliteDb *SqliteDb) SelectTag(tagId int) (*Tag, error) {
 	return readSingleRowTag(row), nil
 }
 
-// func (sqliteDb *SqliteDb) InsertTag(name string) {
-// 	_, err := sqliteDb.db.Exec("INSERT INTO tags  (name) VALUES ('Important')", name)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// }
-
+// todo make color hexadicimal
 func (sqliteDb *SqliteDb) InsertTag(name string, color string) {
 	fmt.Println("Inserting tag")
-	_, err := sqliteDb.db.Exec("INSERT INTO tags  (name, color) VALUES (?, ?)", name, color)
+	_, err := sqliteDb.db.Exec("INSERT INTO tag (name, color) VALUES (?, ?)", name, color)
 	if err != nil {
 		panic(err)
 	}
+}
+
+// folder
+
+func (sqliteDb *SqliteDb) AddFolder(path string) error {
+	_, err := sqliteDb.db.Exec("INSERT INTO folder (folder_path) VALUES (?)", path)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// file
+
+func (sqliteDb *SqliteDb) AddFile(fileName string, folderId int, label string) error {
+	_, err := sqliteDb.db.Exec("INSERT INTO file (file_name, fk_folder_id, label) VALUES (?, ?, ?)", fileName, folderId, label)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// l_file_tag
+
+func (sqliteDb *SqliteDb) AddTagToFile(fileId int, tagId int) error {
+	_, err := sqliteDb.db.Exec("INSERT INTO l_file_tag (fk_file_id, fk_tag_id) VALUES (?, ?)", fileId, tagId)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (sqliteDb *SqliteDb) RemoveTagFromFile(fileId int, tagId int) error {
+	_, err := sqliteDb.db.Exec("DELETE FROM l_file_tag WHERE fk_file_id = ? AND fk_tag_id = ?", fileId, tagId)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
