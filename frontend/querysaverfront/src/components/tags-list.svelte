0e816ff53de8aed8abe1c9e63fe814fbe4d60ef7
NinjaPhillipe <23127972+NinjaPhillipe@users.noreply.github.com>
2023-12-18 00:37:42 +0100

    [FEATURE] Add selected tag list

--- frontend/querysaverfront/src/components/tags-list.svelte
+++ frontend/querysaverfront/src/components/tags-list.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  import { writable } from "svelte/store";
 
   // Define the type for a tag
   interface Tag {
@@ -10,19 +11,45 @@
 
   let tags: Tag[] = [];
 
+  // Create a store for the selected tag
+  export const selectedTags = writable<Tag[]>([]);
+
   onMount(async () => {
     const response = await fetch("http://localhost:8080/tags/query/all");
     tags = await response.json();
     console.log("tags");
     console.log(tags);
   });
+
+  function updateSelectedTagsList(tag: Tag) {
+    console.log("selectedTag");
+    console.log(tag);
+
+    selectedTags.update((value) => {
+      // Check if the tag is already in the store
+      if (!value.some((t) => t.id === tag.id)) {
+        // If it's not in the store, add it
+        return [...value, tag];
+      }
+      // If it's already in the store, return the current value
+      return value;
+    });
+  }
+
+  // onclick;
 </script>
 
-<div class="tag-list">
-  <h2>TAGS LIST</h2>
+<div class="tag-list border-2 flex flex-wrap">
   {#each tags as tag (tag)}
-    <span class="badge" style="background-color: {tag.color};">{tag.name}</span>
+    <span
+      class="badge"
+      on:click={() => updateSelectedTagsList(tag)}
+      style="background-color: {tag.color};">{tag.name}</span
+    >
   {/each}
+</div>
+<div>
+  <p>Selected tags: {$selectedTags.map((tag) => tag.name).join(", ")}</p>
 </div>
 
 <style>
