03a922898ea8b88c781a007155ede5b5992eb562
NinjaPhillipe <23127972+NinjaPhillipe@users.noreply.github.com>
2023-12-20 02:33:39 +0100

    [FEATURE] add btn for type of search query

--- frontend/querysaverfront/src/components/search-state.svelte
+++ frontend/querysaverfront/src/components/search-state.svelte
@@ -1,6 +1,16 @@
 <script lang="ts">
+  import { SlideToggle } from "@skeletonlabs/skeleton";
   import type { Tag } from "../lib/dataType";
   import { selectedTags } from "../lib/store";
+
+  // type of query
+  let orQuery = true;
+  let andQuery = !orQuery;
+
+  function toggleQuery() {
+    orQuery = !orQuery;
+    andQuery = !andQuery;
+  }
 
   function removeTag(tagToRemove: Tag) {
     selectedTags.update((tags) =>
@@ -10,14 +20,24 @@
 </script>
 
 <div>
-  {#each $selectedTags as tag (tag)}
-    <div class="badge" style="background-color: {tag.color};">
-      {tag.name}
-      <button
-        type="button"
-        class="btn btn-sm variant-filled w-2 h-6 ml-2"
-        on:click={() => removeTag(tag)}>x</button
-      >
-    </div>
-  {/each}
+  <div>
+    {#each $selectedTags as tag (tag)}
+      <div class="badge" style="background-color: {tag.color};">
+        {tag.name}
+        <button
+          type="button"
+          class="btn btn-sm variant-filled w-2 h-6 ml-2"
+          on:click={() => removeTag(tag)}>x</button
+        >
+      </div>
+    {/each}
+  </div>
+  <div class="flex flex-col">
+    <SlideToggle name="or" checked={orQuery} on:change={toggleQuery}
+      >OR</SlideToggle
+    >
+    <SlideToggle name="and" checked={andQuery} on:change={toggleQuery}
+      >AND</SlideToggle
+    >
+  </div>
 </div>
